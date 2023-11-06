3d053878dccee378e6a5d307bbef973ec2c3e126
Adam Filchoir <filchoiradam@gmail.com>
2023-11-06 22:03:35 +0700

    Tooltip 1

--- src/components/footer/TinyFooter.vue
+++ src/components/footer/TinyFooter.vue
@@ -2,14 +2,26 @@
   <div class="footer-section">
     <div class="footer-body">
       <div class="copyright">
-        <p>// Made with <span>Love</span> by Adam</p>
+        <p>
+          // Made with
+          <span class="footer-highlight"
+            >Love
+            <!-- <span class="tooltiptext">*Lub* *Dub*</span> -->
+          </span>
+          by Adam
+        </p>
       </div>
       <div class="sign-off">
         <!-- <div></div> Some buttons in the right hand side of the footer -->
         <div>
           <p>
-            "Power is given to those who <span>dare</span> to lower themselves
-            to pick it up" //
+            "Power is given to those who
+            <span class="footer-highlight"
+              >dare<span v-on:mousemove="fn()" class="footer-book-tooltip">
+                Fyodor Dostoyevsky's, Crime and Punishment
+              </span></span
+            >
+            to lower themselves to pick it up" //
           </p>
         </div>
       </div>
@@ -17,7 +29,22 @@
   </div>
 </template>
 
-<script setup></script>
+<script>
+// document.addEventListener("mousemove", fn, false);
+
+export default {
+  mounted() {
+    const tooltips = document.querySelectorAll(".footer-book-tooltip");
+
+    document.addEventListener("mousemove", (e) => {
+      tooltips.forEach((tooltip) => {
+        tooltip.style.left = e.pageX - 175 + "px";
+        tooltip.style.top = e.pageY - 50 + "px";
+      });
+    });
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import "../../assets/shared.scss";
@@ -39,7 +66,7 @@
   color: #efefef;
 }
 
-span {
+.footer-highlight {
   color: #d11b1bfc;
   letter-spacing: 1px;
   font-weight: 700;
@@ -50,9 +77,24 @@ p {
   text-shadow: 0px 0px 5px #363434;
 }
 
-.copyright {
+.footer-highlight:hover .footer-book-tooltip {
+  display: block;
 }
 
-.sign-off {
+.footer-highlight {
+  transition: display 0.2s;
+}
+
+.footer-book-tooltip {
+  background-color: #fff;
+  border-radius: 15px;
+  font-size: 14px;
+  padding: 5px;
+  display: none;
+  position: absolute;
+  text-decoration: none;
+  text-shadow: 0px 0px 10px #ffffff;
+  color: #121212;
+  transition: display 0.2s;
 }
 </style>
